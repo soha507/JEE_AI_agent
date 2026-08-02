fd1d3dde148acf51cd9a6bc3727ebb86ee524cd0
soha507 <109614125+soha507@users.noreply.github.com>
2026-08-02 23:09:00 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -75,7 +75,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent",,
       {
         method: "POST",
         headers: {
